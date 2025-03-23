1803ae3a0bd86b3a36d01970784a630f0764b4a0
aleenaaahad <aleenaahad5@gmail.com>
2025-03-23 16:41:16 -0700

    Updated Contract Name

--- contracts/Contract.sol
+++ contracts/Contract.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.20; //the version of solidity compiler will use 
 
-contract Contract {
-string public print= "hello world"; 
+contract TrackShipment {
 }
+
 
