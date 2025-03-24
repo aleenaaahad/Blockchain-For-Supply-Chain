9c414c1e2e9190ed18bf112315c27bf725dbdf79
aleenaaahad <aleenaahad5@gmail.com>
2025-03-24 15:25:38 -0700

    adding imports

--- contracts/Contract.sol
+++ contracts/Contract.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.20; //the version of solidity compiler will use 
 
+import "hardhat/console.sol"; //console to see smart contract outputs 
+
 contract TrackShipment {
 }
 
