e2386b609b2836f17d5bacb5aae2d643ca798af4
aleenaaahad <aleenaahad5@gmail.com>
2025-04-21 12:53:12 -0700

    Created a structure for Product&Transaction

--- contracts/Contract.sol
+++ contracts/Contract.sol
@@ -4,6 +4,16 @@ pragma solidity ^0.8.20; //the version of solidity compiler will use
 import "hardhat/console.sol"; //console to see smart contract outputs 
 
 contract TrackShipment {
+    struct Product {
+        uint ID;
+        uint Inventory_Level;
+
+    }
+    struct Transaction {
+        uint User_Transaction_Amount;
+        string Logistics_Delay_Reason;
+        uint Timestamp;
+    }
 }
 
 
