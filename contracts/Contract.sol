01789d3c04411b169234758c4bfda435dd9267c0
aleenaaahad <aleenaahad5@gmail.com>
2025-04-27 12:20:31 -0700

    add transaction

--- contracts/Contract.sol
+++ contracts/Contract.sol
@@ -30,6 +30,30 @@ mapping(uint => Transaction[]) public productTransactions;
 function addProduct(uint _ID, uint _Inventory_Level, string memory _Shipment_Status) public {
     products[_ID] = Product(_ID, _Inventory_Level, _Shipment_Status);
 }
+function addTransaction(
+    uint _productID,
+    uint _User_Transaction_Amount,
+    bool _Logistics_Delay,
+    string memory _Logistics_Delay_Reason,
+    string memory _location,
+    string memory _Shipment_Status,
+    string memory _Asset_ID,
+    address _receiver
+) public {
+    Transaction memory newTransaction = Transaction({
+        User_Transaction_Amount: _User_Transaction_Amount,
+        Logistics_Delay: _Logistics_Delay,
+        Logistics_Delay_Reason: _Logistics_Delay_Reason,
+        Timestamp: block.timestamp,
+        sender: msg.sender,
+        receiver: _receiver,
+        location: _location,
+        Shipment_Status: _Shipment_Status,
+        Asset_ID: _Asset_ID
+    });
+
+    productTransactions[_productID].push(newTransaction);
+}
 
 
 }
